d9be7d9a8022aa09167ca5b2986e147ba72e4bd4
user.name "Domasiedu <user.email "domasiedu18@gmail.com>
2026-04-10 01:23:27 -0230

    checkmateUpdate

--- server/chess/isCheckmate.js
+++ server/chess/isCheckmate.js
@@ -1,14 +1,49 @@
 const isKingInCheck = require("./isKingInCheck");
-const getAllPossibleMoves = require("./getAllPossibleMoves");
+const parseFEN = require("./boardParser");
 
-function isCheckmate(fen, color) {
-  if (!isKingInCheck(fen, color)) {
+function isCheckmate(board, color) {
+  const getLegalMoves =
+    require("./getLegalMoves");
+
+  const boardState =
+    typeof board === "string"
+      ? parseFEN(board)
+      : board;
+
+  if (!isKingInCheck(boardState, color)) {
     return false;
   }
 
-  const legalMoves = getAllPossibleMoves(fen, color);
+  for (let row = 0; row < 8; row += 1) {
+    for (let col = 0; col < 8; col += 1) {
+      const piece =
+        boardState[row][col];
 
-  return legalMoves.length === 0;
+      if (!piece) continue;
+
+      const pieceColor =
+        piece === piece.toUpperCase()
+          ? "white"
+          : "black";
+
+      if (pieceColor !== color) continue;
+
+      const moves =
+        getLegalMoves(
+          boardState,
+          row,
+          col,
+          piece
+        );
+
+      if (moves.length > 0) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
-module.exports = isCheckmate;
+module.exports =
+  isCheckmate;
